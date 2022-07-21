59bff63b91fe0ef9dd359ab886a43d48e596f37b
Clement Bailly <cby9@3ds.com>
2022-07-21 15:11:45 +0200

    [New] Beggin

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="">
+    <h1>Index</h1>
+    <NuxtLink to="/test">
+      Ici le test
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+
+</script>
