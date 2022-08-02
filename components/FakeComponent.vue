54bdbfb720f1140340f1c824a0e1991a536c336e
Clement Bailly <cby9@3ds.com>
2022-08-02 22:04:26 +0200

    fix css

--- components/FakeComponent.vue
+++ components/FakeComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full my-12 mx-auto">
+  <div class="border border-blue-300 shadow rounded-md p-4 w-2/5 my-12 mx-auto">
     <div class="animate-pulse flex space-x-4">
       <div class="rounded-full bg-gray-200 h-10 w-10" />
       <div class="flex-1 space-y-6 py-1">
